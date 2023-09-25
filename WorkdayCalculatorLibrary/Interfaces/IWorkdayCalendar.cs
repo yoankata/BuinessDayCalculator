785a981ddd288b9caa6522df491b1acb8758f98b
yoankata <32326965+yoankata@users.noreply.github.com>
2023-09-26 00:04:39 +0300

    Add files via upload
    Initial.

---
+++ WorkdayCalculatorLibrary/Interfaces/IWorkdayCalendar.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkdayCalculatorLibrary.Interfaces
+{
+    public interface IWorkdayCalendar
+    {
+        void SetHoliday(DateTime date);
+        void SetRecurringHoliday(int month, int day);
+        void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes);
+        DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays);
+    }
+
+}
